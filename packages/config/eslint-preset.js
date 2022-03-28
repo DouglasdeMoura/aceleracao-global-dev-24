10f3bfc2ea34d1d88c48e0766d331106bf3d0c4c
Douglas Moura <douglas.ademoura@gmail.com>
2022-03-28 20:10:10 -0300

    chore: adiciona configuração do ESLint

--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,12 +1,102 @@
 module.exports = {
-  extends: ["next", "prettier"],
+  env: {
+    browser: true,
+    node: true,
+  },
   settings: {
-    next: {
-      rootDir: ["apps/*/", "packages/*/"],
+    react: {
+      version: 'detect',
+    },
+    node: {
+      extensions: ['.js', '.jsx', '.json', '.node', '.ts', '.tsx'],
+      project: ['tsconfig.json', 'package/tsconfig.json'],
+    },
+    typescript: {
+      alwaysTryTypes: true,
+      project: ['tsconfig.json', 'package/tsconfig.json'],
     },
   },
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-};
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+      {
+        usePrettierrc: false,
+      },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^react/',
+          'module',
+          '/^styles.*/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+  },
+  overrides: [
+    {
+      env: {
+        jest: true,
+      },
+      files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:jest/recommended', 'plugin:testing-library/react'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'off',
+          {
+            devDependencies: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+          },
+        ],
+      },
+    },
+    {
+      env: {
+        'cypress/globals': true
+      },
+      files: ['**/?(*.)+e2e.[jt]s?(x)'],
+      extends: ['plugin:cypress/recommended'],
+    },
+  ],
+}
