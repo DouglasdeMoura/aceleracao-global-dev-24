e55ea0a89a6b92714c8c057da5ea0c0a141fadb4
Douglas Moura <douglas.ademoura@gmail.com>
2022-04-06 21:21:25 -0300

    chore: upgrade dependencies

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,6 +1,8 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './app'
 
-const app = document.getElementById('app')
-ReactDOM.render(<App />, app)
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+root.render(<App />)
